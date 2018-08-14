ed779ea20561f95b0c0ab893faa00748d8468e9d
jackcooper2015 <1040078821@qq.com>
2018-08-14 10:40:17 +0800

    --自定义模板

--- README.md
+++ README.md
@@ -18,9 +18,27 @@
 
 ##使用说明
 1. 从doc目录找到jar文件，运行命令 nohup java -jar code-*.jar &
+![image.png](https://upload-images.jianshu.io/upload_images/3710706-23fbd71878ec41fe.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
 2. 访问ip:8088
-2. 登录之后默认进入数据库列表页面，如果没有你需要的，请新增数据源
+![image.png](https://upload-images.jianshu.io/upload_images/3710706-6cec5c85b906637c.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+2. 登录之后默认进入数据库列表页面，如果没有你需要的，请新增数据源，测试并保存
+![image.png](https://upload-images.jianshu.io/upload_images/3710706-617d979cd469b4b4.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
 4. 找到你表所在的数据库，点击“选中”，此时可以看到你所有的表信息
+![image.png](https://upload-images.jianshu.io/upload_images/3710706-54390840abcc4dfb.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
 6. 选则你需要生成的表
 7. 添加包名、字段注释后点击保存（写库，不会丢失，下次可以继续使用）
+![image.png](https://upload-images.jianshu.io/upload_images/3710706-4c5a74f4ecba782f.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
 8. 点击生成代码
+![image.png](https://upload-images.jianshu.io/upload_images/3710706-a3ed843e9ec73e15.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+---
+作者相关：
+    
+    简书：https://www.jianshu.com/u/8523e601fa4e
+    
+    github: https://github.com/jackcooper2015
--- src/main/java/com/reapal/CodeGeneratorApplication.java
+++ src/main/java/com/reapal/CodeGeneratorApplication.java
@@ -1,20 +1,35 @@
 package com.reapal;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Controller
 @EnableWebMvc
 @ServletComponentScan
 @SpringBootApplication
+@Slf4j
 public class CodeGeneratorApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		SpringApplication.run(CodeGeneratorApplication.class, args);
+		log.info("\n======================================================\n\t"+
+					"Application is running !!! \n\t"+
+					"Auth: \tjackcooper\n\t"+
+					"Email: \t1040078821@qq.com\n\t"+
+					"Access URLs:\t http://{}:8088\n"+
+					"======================================================",
+				InetAddress.getLocalHost().getHostAddress()
+		);
 	}
 
 
