8af18f91f4b3dea7d098bd943e9050163dff641f
jackcooper2015 <1040078821@qq.com>
2020-03-30 11:35:14 +0800

    初始化数据清空

--- src/main/resources/sql/data.sql
+++
@@ -1,3 +0,0 @@
--- 密码加密方式sha1
-insert ignore into db_config (id,db_name,url,driver,username,password,schema)
-values(1,''aaa'',''jdbc:mysql://ip:port/test?serverTimezone=UTC'',''com.mysql.jdbc.Driver'',''root'',''root'',''test'');
